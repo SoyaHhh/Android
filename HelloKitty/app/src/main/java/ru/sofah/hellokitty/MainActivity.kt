c875f19339b7e060ba2176ae40a8573428c5fd08
Sofya <sofassssss389@gmail.com>
2023-12-18 20:05:56 +0600

    Lesson2

---
+++ HelloKitty/app/src/main/java/ru/sofah/hellokitty/MainActivity.kt
@@ -0,0 +1,33 @@
+package ru.sofah.hellokitty
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var helloTextView: TextView
+    private lateinit var editText: EditText
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        helloTextView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editText)
+
+        val imageButton: ImageButton = findViewById(R.id.imageButton)
+
+        imageButton.setOnClickListener {
+            if (editText.text.isEmpty()) {
+                helloTextView.text = "Hello Kitty!"
+            } else {
+                helloTextView.text = "Привет, " + editText.text
+            }
+        }
+    }
+}
